e013e15117b9f341844f794a36d5454cc3a1a693
gregm711 <miller.7285osu@gmail.com>
2016-12-14 16:56:12 -0500

    added in location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 
 console.log("starting redux");
@@ -12,35 +13,94 @@ var stateDefault = {
 
 var nextHobbyId = 1;
 
-var reducer = (state = stateDefault, action) =>  {
-    console.log("action", action);
-    switch (action.type) {
-        case "CHANGE_NAME":
-            return  {
-                ...state,
-                name: action.name
-            };
-        case "ADD_HOBBY":
-            return {
-                ...state,
-                    hobbies: [
-                    ...state.hobbies,
-                        {
-                            id: nextHobbyId++,
-                            hobby: action.hobby
-                        }
-                    ]
-            };
 
-        case "REMOVE_HOBBY":
+var nameReducer = (state = 'Ananoymouse', action) => {
+    switch(action.type) {
+        case 'CHANGE_NAME':
+            return action.name;
+        default:
+            return state;
+    }
+
+};
+
+var changeName = (name) => {
+    return {
+        type: "CHANGE_NAME",
+        name
+    }
+};
+
+
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
             return {
-                ...state,
-                hobbies: state.hobbies.filter((hobby) =>  hobby.id !== action.id)
+                isFetching: true,
+                url: undefined
+
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return  {
+                isFetching: false,
+                url: action.url
             };
         default:
             return state;
     }
+
 };
+
+
+var startLocationFetch  =  () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+
+    };
+};
+
+var completeLocationFetch  =  (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    };
+};
+
+var fetchLocation = () =>  {
+    store.dispatch(startLocationFetch());
+
+    axios.get("http://ipinfo.io").then(function (res) {
+        var loc = res.data.loc;
+        var baseUrl = "http://maps.google.com?q=";
+        store.dispatch(completeLocationFetch(baseUrl + loc));
+
+
+    });
+};
+
+var hobbiesReducer = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_HOBBY":
+        return  [
+            ...state,
+            {
+                id: nextHobbyId++,
+                hobby: action.hobby
+            }
+        ];
+        case "REMOVE_HOBBY":
+            return state.filter((hobby) =>  hobby.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+var reducer = redux.combineReducers({
+    name: nameReducer,
+    hobbies: hobbiesReducer,
+    map: mapReducer
+
+});
 
 var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -53,38 +113,28 @@ console.log('current state', currentState);
 
 
 
-var action = {
-    type: "CHANGE_NAME",
-    name: "Greg"
-};
-
 var unsubscribe = store.subscribe(() =>  {
     var state = store.getState();
     console.log("name is ", state.name);
     document.getElementById('app').innerHTML = state.name;
+
+    if (state.map.isFetching) {
+        document.getElementById('app').innerHTML = 'loading...';
+
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '"  target="_blank">View your location</a>'
+    }
 });
 
-store.dispatch({
-    type: "CHANGE_NAME",
-    name: "Greg"
 
-});
-// unsubscribe();
+store.dispatch(changeName('Bob'));
 
-store.dispatch({
-    type: "CHANGE_NAME",
-    name: "andrew"
-
-});
+fetchLocation();
 
 store.dispatch({
     type: "ADD_HOBBY",
     hobby: "coding"
-
 });
-
-
-
 
 store.dispatch({
     type: "ADD_HOBBY",
@@ -95,7 +145,7 @@ store.dispatch({
 
 store.dispatch({
     type: "REMOVE_HOBBY",
-    id: 2
+    id: 1
 });
 
 console.log("name ", store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -26,16 +26,17 @@ var reducer = (state = stateDefault, action) =>  {
     }
 };
 
-
 var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
 
-store.subscribe(() =>  {
+var unsubscribe = store.subscribe(() =>  {
     var state = store.getState();
     document.getElementById('app').innerHTML = state.searchText;
 });
+
+unsubscribe();
 
 store.dispatch({
     type: "CHANGE_SEARCH_TEXT",
